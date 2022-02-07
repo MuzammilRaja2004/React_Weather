b34e7707bd5102050d917e20ee7abd50084ab8fc
Muzammil Raja <munnaraja852@gmail.com>
2022-02-07 16:12:31 +0500

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [city, setCity] = useState(null);
+  const [search, setSearch] = useState("karachi");
+
+  useEffect(() => {
+    getWeatherData();
+  });
+  const getWeatherData = async () => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=d250eebb3ee38818ac4098794ece0b0f`;
+    const response = await fetch(url);
+    const resJson = await response.json();
+    // console.log(resJson);
+    setCity(resJson.main);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="main-div">
+        <div className="inner-div">
+          <div className="search">
+            <input
+              type="search"
+              placeholder="Search"
+              value={search}
+              onChange={(e) => {
+                setSearch(e.target.value);
+              }}
+            />
+          </div>
+
+          {!city ? (
+            <p>Not Data Found</p>
+          ) : (
+            <div className="data-view">
+              <h1>{search}</h1>
+              <h1>{city.temp}</h1>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
